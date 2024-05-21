aed092ed3b8b8ba8b4ea0cf514569904abfb9083
Bart <bartolomej.kozorog@gmail.com>
2024-05-22 01:27:59 +0200

    hook to execute api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,10 +8,30 @@ import {ProjectFile, ProjectService} from "@/common/project.service.ts";
 function App() {
     const LANGUAGE_CADENCE = 'cadence';
     const [code, setCode] = useState('');
+    const [args, setArgs] = useState('');
     const [projectFiles, setProjectFiles] = useState<ProjectFile[]>();
+    const [executionResult, setExecutionResult] = useState<unknown>();
     const service = new ProjectService({baseUrl: "http://localhost:8080"});
     const urlParams = new URLSearchParams(window.location.search);
     const projectUrl = urlParams.get('projectUrl');
+
+    async function onExecute() {
+        const isScript = code.includes("pub fun main");
+        const isTransaction = code.includes("transaction");
+
+        // A very dump heuristic to determine if Cadence code is a transaction or script
+        if (isScript) {
+            setExecutionResult(await service.executeScript({
+                source: code,
+                arguments: args
+            }))
+        } else if (isTransaction) {
+            setExecutionResult(await service.executeTransaction({
+                source: code,
+                arguments: args
+            }))
+        }
+    }
 
     useEffect(() => {
         (async function () {
@@ -36,41 +56,44 @@ function App() {
 
     return (
         <div className='flex flex-row'>
-            <div className="flex flex-col justify-between p-2">
-                <div className="flex flex-col gap-y-[10px]">
-                    {projectFiles
-                        ?.filter(file => !file.isDirectory)
-                        .map(file => {
-                            const fileName = file.path.split("/").reverse()[0]
-                            return (
-                                <div key={file.path} onClick={() => setCode(file.content)}
-                                     className="max-w-[200px] truncate text-left">
-                                    {fileName}
-                                </div>
-                            )
-                        })}
-                </div>
+            <div className="flex flex-col gap-y-[10px] p-2">
+                {projectFiles
+                    ?.filter(file => !file.isDirectory)
+                    .map(file => {
+                        const fileName = file.path.split("/").reverse()[0]
+                        return (
+                            <div key={file.path} onClick={() => setCode(file.content)}
+                                 className="max-w-[200px] truncate text-left">
+                                {fileName}
+                            </div>
+                        )
+                    })}
+            </div>
 
-                {code && (
+            <div className="flex flex-col w-full">
+                <Editor
+                    theme='vs-dark'
+                    language={LANGUAGE_CADENCE}
+                    value={code}
+                    onChange={code => setCode(code ?? "")}
+                    className="h-[60vh] pt-2 w-full"
+                    options={{automaticLayout: true}}
+                    beforeMount={beforeEditorMount}
+                />
+
+                <div className="h-[40vh] flex flex-row">
+                    <pre>
+                        {JSON.stringify(executionResult, null, 4)}
+                    </pre>
                     <div>
                         <label>
                             Arguments
-                            <textarea rows={10}></textarea>
+                            <textarea rows={10} value={args} onChange={e => setArgs(e.target.value)}></textarea>
                         </label>
-                        <button>Execute</button>
+                        <button onClick={onExecute}>Execute</button>
                     </div>
-                )}
+                </div>
             </div>
-
-            <Editor
-                theme='vs-dark'
-                language={LANGUAGE_CADENCE}
-                value={code}
-                onChange={code => setCode(code ?? "")}
-                className="h-[100vh] pt-2 w-full"
-                options={{automaticLayout: true}}
-                beforeMount={beforeEditorMount}
-            />
         </div>
     )
 }
